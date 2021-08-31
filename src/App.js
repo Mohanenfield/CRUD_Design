3f7d459a650aa9b00710dafb30e07bd59a62b116
Mohanenfield <83965863+Mohanenfield@users.noreply.github.com>
2021-08-31 11:51:12 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,12 @@
 import { BrowserRouter, Route, Switch, Redirect, Link } from "react-router-dom";
 import { Table } from "react-bootstrap";
-import Posts from "./Posts";
+import PostApp from "./Posts";
 
 function App() {
     return (
-        <BrowserRouter>
-            <Table striped bordered hover>
-                <thead>
-                    <th><Link to="/posts">Posts</Link></th>
-                </thead>
-            </Table>
-            <Switch>
-                <Route path="/posts">
-                    <Posts />
-                </Route>
-            </Switch>
-        </BrowserRouter>
+        <>
+        <PostApp />
+         </>   
     );
 }
 export default App;
